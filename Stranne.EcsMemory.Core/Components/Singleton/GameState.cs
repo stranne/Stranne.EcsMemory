ac7c16090860b403f55aef6536db338883a51686
Olof Stranne <olof.stranne@gmail.com>
2025-08-17 02:43:46 +0200

    chore: simplify components data

--- Stranne.EcsMemory.Core/Components/Singleton/GameState.cs
+++ Stranne.EcsMemory.Core/Components/Singleton/GameState.cs
@@ -3,7 +3,6 @@ namespace Stranne.EcsMemory.Core.Components.Singleton;
 using Arch.Core;
 
 internal struct GameState {
-  public int FlipsThisTurn;
   public int Moves;
   public bool IsLocked;
   public Entity? FirstFlipped;
