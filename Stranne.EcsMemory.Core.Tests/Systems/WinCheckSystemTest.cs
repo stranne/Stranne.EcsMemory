05fc92d8d415307974b81e8bcb2bbecdb42e1962
Olof Stranne <olof.stranne@gmail.com>
2025-08-24 21:49:46 +0200

    feat: test won event + fix broken unit tests

--- Stranne.EcsMemory.Core.Tests/Systems/WinCheckSystemTest.cs
+++ Stranne.EcsMemory.Core.Tests/Systems/WinCheckSystemTest.cs
@@ -1,6 +1,7 @@
 ﻿using Arch.Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using Stranne.EcsMemory.Core.Components.Events;
 using Stranne.EcsMemory.Core.Components.Singleton;
 using Stranne.EcsMemory.Core.Extensions;
 using Stranne.EcsMemory.Core.Systems;
@@ -9,6 +10,7 @@ using Stranne.EcsMemory.Core.Tests.Common;
 namespace Stranne.EcsMemory.Core.Tests.Systems;
 internal sealed class WinCheckSystemTest
 {
+    private static readonly QueryDescription WonEventQuery = new QueryDescription().WithAll<EventWon, EventMetadata>();
     private static readonly ILogger<WinCheckSystem> Logger = new NullLogger<WinCheckSystem>();
 
     [Test]
@@ -19,7 +21,7 @@ internal sealed class WinCheckSystemTest
 
         sut.Update(0);
 
-        await AssertState(world, true, true);
+        await AssertState(world, true, true, true);
     }
 
     [Test]
@@ -36,7 +38,7 @@ internal sealed class WinCheckSystemTest
     [Test]
     public async Task WinCheck_Skip_WhenTotalCardsAreZero()
     {
-        var world = TestWorldFactory.Create(totalCards: 0, matchedCount: 10);
+        var world = TestWorldFactory.Create(totalCards: 0, matchedCount: 0);
         var sut = new WinCheckSystem(world, Logger);
 
         sut.Update(0);
@@ -47,20 +49,23 @@ internal sealed class WinCheckSystemTest
     [Test]
     public async Task WinCheck_Skip_WhenNotAllCardsAreMatched()
     {
-        var world = TestWorldFactory.Create(totalCards: 8, matchedCount: 10);
+        var world = TestWorldFactory.Create(totalCards: 10, matchedCount: 8);
         var sut = new WinCheckSystem(world, Logger);
         sut.Update(0);
         
         await AssertState(world);
     }
 
-    private static async Task AssertState(World world, bool isWon = false, bool isLocked = false)
+    private static async Task AssertState(World world, bool isWon = false, bool isLocked = false, bool expectWonEvent = false)
     {
         using (Assert.Multiple())
         {
             var gameState = world.GetSingletonRef<GameState>();
-            await Assert.That(isWon).IsEqualTo(isWon);
-            await Assert.That(isLocked).IsEqualTo(isLocked);
+            await Assert.That(gameState.IsWon).IsEqualTo(isWon);
+            await Assert.That(gameState.IsLocked).IsEqualTo(isLocked);
+
+            var eventWonCount = world.CountEntities(in WonEventQuery);
+            await Assert.That(eventWonCount).IsEqualTo(expectWonEvent ? 1 : 0);
         }
     }
 }
